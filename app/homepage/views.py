c971522f95694f2808b39322e8dc3275c79f6b8d
Cha245689 <chaiya45689@gmail.com>
2016-11-03 15:08:27 +0700

    redirect

--- app/homepage/views.py
+++ app/homepage/views.py
@@ -1,12 +1,14 @@
 from flask import Flask,Blueprint, render_template
 
 # create Blueprint class with name importname Blueprintfolders
-Homepage = Blueprint('homepage',__name__,url_prefix='/<user_url_slug>',template_folder='',static_folder='')
+Homepage = Blueprint('homepage',__name__,template_folder='',static_folder='')
 
-@Homepage.url_value_preprocessor
-def user_id(endpoint,id):
-    pass
+#still figuring it
+#@Homepage.url_value_preprocessor
+#def user_id(endpoint,id):
+#    pass
 
+#Homepage_ waiting for template
 @Homepage.route('/Home')
 def Home():
-    return 0
+    return "hah"
