7ec238bdea5ba4b1831bac2d267d8e50b9ddc342
Cha245689 <chaiya45689@gmail.com>
2016-11-04 18:20:10 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/login/login_from_w3.html

--- app/homepage/views.py
+++ app/homepage/views.py
@@ -1,14 +1,16 @@
-from flask import Flask,Blueprint, render_template
+from flask import Flask,Blueprint, render_template,g,request,json,jsonify
+import sqlite3
 
 # create Blueprint class with name importname Blueprintfolders
-Homepage = Blueprint('homepage',__name__,template_folder='',static_folder='')
+Homepage = Blueprint('homepage',__name__,url_prefix="/<url_user_id>",template_folder='',static_folder='')
 
 #still figuring it
-#@Homepage.url_value_preprocessor
-#def user_id(endpoint,id):
-#    pass
+@Homepage.url_value_preprocessor
+def user_id(endpoint,url_user_id):
+    print url_user_id
+    g.id = url_user_id
 
 #Homepage_ waiting for template
 @Homepage.route('/Home')
-def Home():
+def Home(url_user_id):
     return "hah"
