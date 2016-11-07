cd0ee7aea7b772c004b462cb5cd889a8ddb9b326
Kitti <bosskitti7983@outlook.com>
2016-11-07 16:57:43 +0700

    add work test in Data.db

--- app/DataEditor.py
+++ app/DataEditor.py
@@ -8,6 +8,11 @@ c= conn.cursor()    #prepare from modify db
 
 
 # create table name user
+
+# c.executescript("""DROP TABLE User""") #delete table
+# c.executescript("""DELETE FROM subject WHERE Subject_ID = 'FRA241'""") #delete data in table subject
+# c.executescript("""DELETE FROM work WHERE Subject_ID ='FRA222'""") #delete work
+
 try :
     c.executescript("""
     CREATE TABLE `User` (
@@ -20,7 +25,7 @@ try :
       `Role` varchar(45) DEFAULT NULL,
       `Faculty` varchar(45) DEFAULT NULL,
       `Major` varchar(45) DEFAULT NULL,
-      `Enro-Yearl` varchar(45) DEFAULT NULL,
+      `Enrol-Year` varchar(45) DEFAULT NULL,
       `Picture` varchar(45) DEFAULT NULL
     )""")
 except Exception:
@@ -113,35 +118,51 @@ except Exception:
     print "Table Work has created"
 
 
-    #insert data in Enrol table
+# insert data in Enrol table
+def EnrolInsert():
     c.execute("""INSERT INTO `Enrol` (`ID`, `Subject_ID`, `subject_Year`) VALUES
     (58340500017, 'FRA222', 59);""")
 
-    #insert data in subject table
+#insert data in subject table
+def subjectInsert():
     c.execute("""INSERT INTO `subject` (`Subject_ID`, `Year`, `Description`, `FullMark`, `Grading`) VALUES
     ('FRA222', 59, NULL, 100, NULL);""")
 
-    # insert data in User table
-    c.execute("""INSERT INTO `User` (`ID`, `Password`, `Title`, `Name`, `Surname`, `E-mail`, `Role`, `Faculty`, `Major`, `Enro-Yearl`, `Picture`) VALUES
+# insert data in User table
+def UserInsert():
+    c.execute("""INSERT INTO `User` (`ID`, `Password`, `Title`, `Name`, `Surname`, `E-mail`, `Role`, `Faculty`, `Major`, `Enrol-Year`, `Picture`) VALUES
     (58340500017, 'Boomming1*', 'Mr.', 'Chaiyaporn', 'Boonyasathian', 'chaiya45689@gmail.com', 'student', 'FIBO', 'robotic and automation', '58', NULL);
     """)
 
-    #insert data in Work table
+#insert data in Work table
+def workInsert():
     c.execute("""INSERT INTO `work` (`Subject_ID`, `Year`, `WorkID`, `Deadlines`, `status`, `type`, `FullMark`, `Grading`, `lim_member`) VALUES
-    ('FRA222', 59, 1, NULL, NULL, NULL, NULL, NULL, NULL);
+    ('FRA221', 59, 5, NULL, NULL, NULL, 10, NULL, NULL);
     """)
 
-conn.commit() #save data into db
+# UserInsert() #insert User
+# subjectInsert() #insert subject
+workInsert() # insert work
 
+conn.commit() #save data into db
+print("-----------User-----------")
 cursor1 = c.execute("SELECT ID, Password from User") #choose table for search data
 for row in cursor1:
     print "ID = ", row[0]
     print "Password = ", row[1]
-
-cursor2 = c.execute("SELECT ID, Subject_ID from Enrol")
+print("-----------subject-----------")
+cursor2 = c.execute("SELECT Subject_ID, Year from subject")
 for row in cursor2:
-    print "ID = ",row[0]
-    print "Subject ID = ",row[1]
+    print "Subject ID = ",row[0]
+    print "Year = ",row[1]
+print("-----------work-----------")
+cursor3 = c.execute("SELECT Subject_ID, Year, WorkID, FullMark from work")
+for row in cursor3:
+    print "Subject ID = ",row[0]
+    print "Year = ",row[1]
+    print "WorkID = ",row[2]
+    print "FullMark = ",row[3]
 
 cursor1.close()
 cursor2.close()
+cursor3.close()
