a6f1c384ca91c052397910f88bb62d0afff3ca4d
Kitti <bosskitti7983@outlook.com>
2016-11-07 18:02:04 +0700

    create add data function in database

--- app/DataEditor.py
+++ app/DataEditor.py
@@ -4,7 +4,6 @@ import sqlite3
 conn = sqlite3.connect('Data.db') #connect db name Data.db
 print "Connect : pass"
 c= conn.cursor()    #prepare from modify db
-null = None
 
 # create table name user
 
@@ -117,30 +116,47 @@ except Exception:
     print "Table Work has created"
 
 
+# insert data in User table
+def UserInsert(ID, Password, Title, Name, Surname, E_mail, Role, Faculty, Major, Enrol_Year, Picture): #Picture default is NULL
+    c.execute("""INSERT INTO `User` (`ID`, `Password`, `Title`, `Name`, `Surname`, `E-mail`, `Role`, `Faculty`, `Major`, `Enrol-Year`, `Picture`) VALUES
+    (?,?,?,?,?,?,?,?,?,?,?);""",(ID, Password, Title, Name, Surname, E_mail, Role, Faculty, Major, Enrol_Year, Picture))
+
 # insert data in Enrol table
 def EnrolInsert(ID,Subject_ID,Subject_Year):
     c.execute("""INSERT INTO `Enrol` (`ID`, `Subject_ID`, `subject_Year`) VALUES
     (?, ?, ?);""",(ID,Subject_ID,Subject_Year))
+
+#insert data in table Group
+def groupInsert(Subject_ID,Year,WorkID,ID):
+    c.execute("""INSERT INTO `group` (`Subject_ID`, `Year`, `WorkID`, `ID,) VALUES
+     (?,?,?,?)""",(Subject_ID,Year,WorkID,ID))
+
+#insert data in table media
+def mediaInsert(Subject_ID,Year,Description,FullMark,Grading):
+    c.execute("""INSERT INTO `media` (`Subject_ID`,`Year`,`Description`,`FullMark`,`Grading`) VALUES
+     (?,?,?,?,?)""",(Subject_ID,Year,Description,FullMark,Grading))
 
 #insert data in subject table
 def subjectInsert(Subject_ID, Year, Description, FullMark, Grading): #Description and Grading default are NULL
     c.execute("""INSERT INTO `subject` (`Subject_ID`, `Year`, `Description`, `FullMark`, `Grading`) VALUES
     (?,?,?,?,?);""",(Subject_ID,Year,Description,FullMark,Grading))
 
-# insert data in User table
-def UserInsert(ID, Password, Title, Name, Surname, E_mail, Role, Faculty, Major, Enrol_Year, Picture): #Picture default is NULL
-    c.execute("""INSERT INTO `User` (`ID`, `Password`, `Title`, `Name`, `Surname`, `E-mail`, `Role`, `Faculty`, `Major`, `Enrol-Year`, `Picture`) VALUES
-    (?,?,?,?,?,?,?,?,?,?,?);""",(ID, Password, Title, Name, Surname, E_mail, Role, Faculty, Major, Enrol_Year, Picture))
+#insert data in Submitwork table
+def subjectInsert(Subject_ID, Year, WorkID, ID, Address, Status, Mark): #Description and Grading default are NULL
+    c.execute("""INSERT INTO `Submitwork` (`Subject_ID`, `Year`, `WorkID`, `ID`, `Address`, `Status`, `Mark`) VALUES
+    (?,?,?,?,?,?,?);""",(Subject_ID, Year, WorkID, ID, Address, Status, Mark))
 
-#insert data in Work table
+#insert data in work table
 def workInsert(Subject_ID, Year, WorkID, Deadlines, status, type, FullMark, Grading, lim_member): #Subject_ID, Year, WorkID aren't NULL
     c.execute("""INSERT INTO `work` (`Subject_ID`, `Year`, `WorkID`, `Deadlines`, `status`, `type`, `FullMark`, `Grading`, `lim_member`) VALUES
     (?,?,?,?,?,?,?,?,?);""",(Subject_ID, Year, WorkID, Deadlines, status, type, FullMark, Grading, lim_member))
 
-# UserInsert() #insert User
-subjectInsert(Subject_ID='FRA142',Year='59',Description=None,FullMark=None,Grading=None) #insert subject
-# workInsert(Subject_ID='FRA221',Year='59',WorkID='6') # insert work
+
+# UserInsert() #insert User #Picture default is NULL
+# subjectInsert(Subject_ID='FRA142',Year='59',Description=None,FullMark=None,Grading=None) #insert subject # Description and Grading default are NULL
+# workInsert(Subject_ID='FRA221',Year='59',WorkID='6') # insert work #Subject_ID, Year, WorkID aren't NULL
 # EnrolInsert('58340500017','FRA221','59')
+
 
 conn.commit() #save data into db
 print("-----------User-----------")
